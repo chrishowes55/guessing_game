ab133de4de492745ab55a18882b4affd46701714
Chris Howes <chrishowes36@gmail.com>
2017-10-05 18:48:22 +0100

    Now generates secret number, although it prints that to screen

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
 use std::io;
+use rand::Rng;
 
 fn main() {
     println!("Guess the number please");
+
+    let secret_number = rand::thread_rng().gen_range(1, 101);
+
+    println!("The secret number is: {}", secret_number);
 
     println!("Please input your guess");
 
@@ -11,6 +18,4 @@ fn main() {
         .expect("Failed to read line");
 
     println!("You guess: {}", guess);
-
-    //got up to Using a Crate to Get More Functionality\\
 }
